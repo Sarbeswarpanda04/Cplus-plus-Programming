4c934f591727f2393816389267d873699203e7c4
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-01-08 20:23:54 +0530

    Add program to calculate the area of a circle based on user input

---
+++ area_of_circle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r;
+    cout<<"Enter radius of circle: ";
+    cin>>r;
+    float area = 3.14 * r * r;
+    cout<< "area is :" << area;
+    return 0;
+}
