5d5930a3485fc9e78cafeeb09034b1cd4f7e47d8
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-01-08 20:03:16 +0530

    Add arithmetic operations program to demonstrate basic calculations

---
+++ arithmatic_operation.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a = 10;
+    int b = 4;
+
+    cout<<a+b<<endl;        // Addition
+    cout<<a-b<<endl;        // Subtraction
+    cout<<a*b<<endl;        // Multiplication
+    cout<<a/b<<endl;        // Division
+
+
+    return 0;
+}
