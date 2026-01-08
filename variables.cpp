7ebb292fc2dcaf4575941ed06712972ce96c4e93
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-01-08 19:53:56 +0530

    Add variables program to demonstrate basic arithmetic operations

---
+++ variables.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int x;
+    x =5;
+    cout<<x;
+
+    x = x+5;
+    cout<<endl << x;
+
+    int y =2;
+    cout<<endl<< x*y;
+
+    return 0;
+}
